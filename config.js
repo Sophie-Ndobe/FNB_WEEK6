2fc0cced00abe2da98b2c1e5857d8a6d10554d4a
Sophie-Ndobe <139694211+Sophie-Ndobe@users.noreply.github.com>
2025-06-15 17:12:08 +0200

    Updated the file with functions to call the json data in the API

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 
 
-let rootPath = "https://mysite.itvarsity.org/api/contactBook/";
+let rootPath = "https://mysite.itvarsity.org/api/ContactBook/";
 let apiKey = checkApiKey();
 
 function checkApiKey() {
-    if (!localStorage.getItem(apiKey)) {
+    if (!localStorage.getItem("apiKey")) {
         window.open("enter-api-key.html", "_self");
     }
     return localStorage.getItem("apiKey");
